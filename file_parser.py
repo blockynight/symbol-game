c377764d57519c18fdc1e093f8169e6f4ad5c9f7
blockynight <joncpim@gmail.com>
2013-02-23 11:56:34 +0000

    Rewrote file_parser, added more tests to test_parser.

--- file_parser.py
+++ file_parser.py
@@ -1,103 +1,138 @@
-# Parses a text file for the symbol game.
-
-import copy # use deep copy.
+# A class for parsing text files, must add write ability to store things.
 
 class Parser(object):
 
-    def __init__(self, DIR):
 
-        self.DIR = DIR                  # Directory to get files from.
-        self.file_data = {}
+    def parse(self, FILE, M, storage):    # FILE path to file to be parsed.
+                                          # M mode to use when opening file.
+        txt = self.open_file(FILE, M)     # storage must be a dictionary.
+        if (None == txt): self.error(txt, "No such file exists.")
 
-
-    def parse(self, FILE, M, storage):       # FILE to be opened, M -mode to use eg 'r'
-        
-        txt = self.open_file(FILE, M)
-        if(None == txt): self.error("No such file exists.")
-        self.mk_list(txt, storage)
-
+        while True:
             
-    def open_file(self, FILE, M):            # Handles if the file to open doesn't exist.
+            print "DEBUG: loop1"
+            line = self.get_line(txt)
+            if (None == line): break
+            if ("" == line): continue
+            if ("#" == line[0]): continue    # ignore comments
+            if ("=" != line[1]): self.error(txt, "No asignment possible.")
+
+            name = line[0]
+
+            if ('"""' in line[2]):
+                info = self.find_set(txt, line[2:], '"""')
+                storage[name] = info
+
+            elif ('/' in line[2]):
+                info = self.find_set(txt, line, '/')
+                storage[name] = info
+
+            else:
+                self.error(txt, "Nothing to store.")
+
+            print "DEBUG2: %s" % info
+
+        txt.close()
+
+
+    def open_file(self, FILE, M):
 
         try:
-            return open(self.DIR + FILE, M)
+            return open(FILE, M)
 
         except IOError:
             return None
 
-
-    def mk_list(self, txt, storage):         # store vars from file in a dictionary
-
-        while True:
-            raw_line = self.get_line(txt)
-            if(None == raw_line): break
-
-            if("" == raw_line): continue
-
-            else:
-                line = raw_line.split()
-                self.store(line, storage, txt)
-                print storage[line[0])
-
-    def get_line(self, txt):                 # Gets next line of file, handles errors.
-
+        
+    def get_line(self, txt):    # Reads a line from the file object passed to it
+                                # and checks for errors and returns the line as a list.
         try:
-            return txt.readline().strip()
+            raw_line = txt.next().strip()
+            line = self.clean_line(raw_line)
+            return line 
 
-        except EOFError:
-            return None
+        except StopIteration:
+            return None 
 
 
-    def store(self, line, storage, txt):
-        
-        asign = self.find_str(line, "=", 0)
-        if (None == asign): self.error("No = char can't read data")
+    def clean_line(self, raw_line):    # Used to determine wheather a line has any 
+                                       # visable chars in it, if theres even one in it
+        proving_table = []             # the whole line is returned as a list.
 
-        info = []
-        
-        du = 0
-        print "DEBUG:", txt.tell()
-        cut_locate1 = self.find_str(line, '"""', du)
-        if (None == cut_locate1): self.error("Nothing to read")
+        for char in raw_line:
+            if (False == char.isalnum()): proving_table.append(False)
+            else: proving_table.append(True)
 
-        info.append("".join(line)[cut_locate1 + 1:])
-        du += 1
-        
-        nline = deepcopy(
-            
-        cut_locate2 = self.find_str(line, '"""', du)
-        if (None == cut_locate2):
+        for value in proving_table:                       # If all values in proving_table
+            if (True == value): return raw_line.split()   # are false, the line has
+                                                          # no visable chars in it. 
+        return ""
+
+
+    def find_set(self, txt, line, string):    # Find the locations of a set eg """ to """
+                                              # and return all thats between them.
+        temp_storage = []
+        print "DEBUG3: Got Here"
+        stage1 = self.find_string(" ".join(line), string, True)
+        if (None == stage1): self.error(txt, "%s isn't there." % string)
+        print "DEBUG: STAGE1:", stage1
+
+        stage2 = self.find_string(stage1, string, True)
+        print "DEBUG: STAGE2:", stage2
+        if (None == stage2):
+            temp_storage.append(stage1)
+
             while True:
-                raw_line = self.get_line(txt)
-                if (None == raw_line): self.error("End of File")
-                    
-                if ("" == raw_line): continue
-                line = raw_line.split()
+                print "DEBUG: looping here or?"
+                nline = self.get_line(txt)
+                if (None == nline): self.error(txt, "Second %s doesn't exist." % string)
+                if ("" == nline): continue
 
-                temp_locate = self.find_str(line, '"""', du)               
-                du += 1
+                stage2 = self.find_string(" ".join(nline), string, True)
+                if (None == stage2):
+                    temp_storage.append("\n" + " ".join(nline))    # Makes sure that the final string
+                    continue                                       # created is correctly formated
 
-                if (None == temp_locate):
-                    info.append("".join(line))
-                    continue
+                else: 
+                    print "DEBUG: STAGE2.2:", nline, stage2
+                    temp_storage.append("\n" + stage2)
+                    print temp_storage                     # debug
+                    break
 
-                cut_locate2 = temp_locate
-                break
+        else:
+            temp_storage.append(stage2)
 
-        info.append("".join(line)[:cutlocate2 + 1])
-                    
-        storage[line[0]] = "".join(info)
+        print "DEBUG4: %s" % " ".join(temp_storage)
+        return " ".join(temp_storage)
 
 
-    def find_str(self, line, string, du):
+    def find_string(self, line, string, cut=False):   # line should be a string never list.
 
         if string in line:
-            return line.index(string, du)
+
+            if (True == cut): return self.cut_line(line, string)
+            else: return line.index(string)
 
         else:
             return None
 
 
-    def error(self, message):
+    def cut_line(self, line, string):
+ 
+        lenstr = len(string)
+        linedex = line.index(string)
+        print "DEBUG", line
+        if (string == line[:linedex + lenstr]): 
+            print line[linedex + lenstr:]                # debug.
+            return line[linedex + lenstr:]
+        else:
+            print line[:linedex]                   # debug
+            return line[:linedex]
 
-        raise Exception(message)
+
+    def error(self, txt, message):
+
+        txt.close()
+        raise  Exception(message)
+            
+            
--- test_parser.py
+++ test_parser.py
@@ -1,11 +1,60 @@
 # A file for testing that the file_parser.py file runs as expected.
 
-from file_parser import *
+from file_parse import *
+import sys 
 
 storage = {}
-
-parser = Parser("levels/")
-parser.parse("test.txt", "r", storage)
+fili = "levels/test.txt"
+m = "r"
+storage = {}
+p = Parser()
+p.parse(fili, m, storage)
 
 print storage["title"]
 print storage["text"]
+print storage
+
+
+
+#======================================================#
+# Testing individual functions.
+
+print "\n\n"
+
+# Cut_line
+f = p.open_file(fili, m)
+l = p.get_line(f)
+print l
+ls = l[2:]
+ln = p.cut_line(" ".join(ls), '"""')
+print ln
+ln2 = p.cut_line(ln, '"""')
+print ln2
+storage[l[0]] = ln2
+print storage                              # cut_line func works.
+
+print ""
+
+# Find_string 
+f.seek(0, 0)
+l = p.get_line(f)
+print l
+ls = l[2:]
+ln = p.find_string(" ".join(ls), '"""', True)
+print ln
+ln2 = p.find_string(ln, '"""', True)
+print ln2
+storage[l[0]] = ln2
+print storage                              # find_string func works.
+
+print ""
+
+# Find_set
+f.seek(0, 0)
+l = p.get_line(f)
+print l
+ls = l[2:]
+ln = p.find_set(f, ls, '"""')
+print ln
+storage[l[0]] = ln
+print storage                              # find_set func works
