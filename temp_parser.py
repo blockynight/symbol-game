fa5386a89a5cfe708db488271f3f6ab93086457f
blockynight <joncpim@gmail.com>
2013-03-02 11:28:57 +0000

    Future additions to file_parser.

---
+++ temp_parser.py
@@ -0,0 +1,29 @@
+# Making the file_parser from symbol game into a full library
+
+class FileParser(object):
+
+    def __init__(self, rules):
+        
+        self.rules = rules
+
+
+    def read_file(self, file_obj, storage):
+
+        pass
+
+
+    def write_file(self, file_obj, storage):
+
+        pass
+
+    def append_file(self, file_obj):
+
+        pass
+
+
+class RulesParser(object):
+
+    def parse_rules(self, rules):    # interpert and apply rules passed to FileParser
+
+        pass
+
