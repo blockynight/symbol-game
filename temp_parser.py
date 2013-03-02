9ee4c7f32a23ec65d8d2bcefb094ee78033b474a
blockynight <joncpim@gmail.com>
2013-03-02 15:04:26 +0000

    Updated basic funcs

--- temp_parser.py
+++ temp_parser.py
@@ -1,5 +1,7 @@
 # Making the file_parser from symbol game into a full library
 
+import os
+
 class FileParser(object):
 
     def __init__(self, rules):
@@ -7,18 +9,47 @@ class FileParser(object):
         self.rules = rules
 
 
-    def read_file(self, file_obj, storage):
+    def read_file(self, input_file, storage):
 
         pass
 
 
-    def write_file(self, file_obj, storage):
+    def write_file(self, input_file, newfile=False, storage):
+        
+        if (0 == len(storage)): self.error(None, "Nothing to write.")
+            
+        file_obj = self.set_up(input_file, "r+", newfile)       
+        file_obj.close()
 
-        pass
 
-    def append_file(self, file_obj):
+    def append_file(self, input_file, newfile=False):
 
-        pass
+        file_obj = self.setup(input_file, "a", newfile)
+
+        file_obj.close()
+
+
+    def set_up(self, input_file, mode, newfile):
+    # check if file is locked check.
+        file_obj = self.open_file(input_file, mode)
+        if (None == file_obj): self.error(None, "Couldn't open file.")
+
+        return file_obj
+
+
+    def open_file(self, input_file, mode):
+
+        try:
+            return open(input_file, mode)
+
+        except IOError:
+            return None
+
+
+    def error(self, file_obj, message):
+
+        if (None != file_obj): file_obj.close()
+        raise Exception(message)
 
 
 class RulesParser(object):
