f3c9934ac821ed5ad673dbc3469814b8f4e8b683
blockynight <joncpim@gmail.com>
2013-03-09 14:44:18 +0000

    Added getting lines.

--- temp_parser.py
+++ temp_parser.py
@@ -13,25 +13,29 @@ class FileParser(object):
 
         file_obj = self.setup(input_file, "r", False)
         
+        info = self.get_line(file_obj)
+        if (None == info): self.error(file_obj, "Reading file failed.")
+   
+        storage["1"] = info
         file_obj.close()
 
 
-    def write_file(self, input_file, newfile=False, storage):
+    def write_file(self, input_file, storage, newfile=False):
         
         if (0 == len(storage)): self.error(None, "Nothing to write.")
             
-        file_obj = self.set_up(input_file, "r+", newfile)       
+        file_obj = self.setup(input_file, "r+", newfile)       
         file_obj.close()
 
 
-    def append_file(self, input_file, newfile=False):
+    def append_file(self, input_file, append_line ,newfile=False): 
 
         file_obj = self.setup(input_file, "a", newfile)
 
         file_obj.close()
 
 
-    def set_up(self, input_file, mode, newfile):
+    def setup(self, input_file, mode, newfile):
     
         test_exists = self.open_file(input_file, "r")
 
@@ -63,6 +67,40 @@ class FileParser(object):
             return None
 
 
+    def get_line(self, file_obj):
+
+        try: 
+            raw_line = file_obj.next().strip()
+            line = self.clean_line(raw_line)
+            return line
+
+        except StopIteration:
+            return None
+
+
+    def clean_line(self, raw_line):
+        """
+        Returns a blank line if the raw_line has no visable 
+        alpha numeric characters.
+        """
+
+        proving_table = []
+
+        for char in raw_line:
+            if (False == char.isalnum): proving_table.append(False)
+            else: proving_table.append(True)
+
+        for value in proving_table:
+            if (True == value): return raw_line
+            
+        return ""
+
+
+    def find_str(self, string, line):
+        
+        pass
+
+
     def error(self, file_obj, message):
 
         if (None != file_obj): file_obj.close()
@@ -75,3 +113,11 @@ class RulesParser(object):
 
         pass
 
+
+p = FileParser("none")
+s = {"a":5}
+f = "levels/test3.txt"
+p.write_file(f, s, True)
+s = {}
+p.read_file(f, s)
+print s
